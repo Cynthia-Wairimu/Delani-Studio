d4d19366be41799cc3581cc179c12e312b3129db
CynthiaWairimu <wairimucee@gmail.com>
2022-03-17 01:10:24 +0300

    correct linking of js to html file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@ var contactForm = $("#contact-form");
 
 $(document).ready(function () {
   // what we do titles add bold class and center
-  $(".w-title").addClass("fw-bold text-center");
+  $(".w-title").addClass("text-center");
 
   contactForm.on("submit", function (e) {
     e.preventDefault();
     let formValid = validateContactForm();
     if (formValid) {
-      const name = $("input.name").val();
+      let name = $("input.name").val();
 
       alert(
         `Thank you ${name}, your message has been received, We'll get back to you shortly.`
@@ -17,51 +17,53 @@ $(document).ready(function () {
     }
   });
 
+
   toggleWhatWeDo();
 });
 
-// function
+  // function
 
-//  function to toggle what we do
-function toggleWhatWeDo() {
-  $(".service").on("click", function () {
-    if ($(".service-details", this).is(":visible")) {
-      $(".service-details", this).hide();
-      $(".service-card", this).fadeIn("slow");
-    } else {
-      $(".service-details", this).fadeIn("slow");
-      $(".service-card", this).hide();
-    }
-  });
-}
+  //  function to toggle what we do
+  function toggleWhatWeDo() {
+    $(".service").on("click", function() {
+      if ($(".service-details", this).is(":visible")) {
+        $(".service-details", this).hide();
+        $(".service-card", this).fadeIn("slow");
+      } else {
+        $(".service-details", this).fadeIn("slow");
+        $(".service-card", this).hide();
+      }
+    });
+  }
 
-// validate contact form
+  // validate contact form
 
-function validateContactForm() {
-  let isFormValid = true;
-  const formInputs = contactForm.find("input,textarea");
+  function validateContactForm() {
+    let isFormValid = true;
+    const formInputs = contactForm.find("input,textarea");
 
-  formInputs.each(function () {
-    if (!$(this).val()) {
-      $(this).addClass("is-invalid");
-      isFormValid = false;
-    }
-  });
-  return isFormValid;
-}
+    formInputs.each(function() {
+      if (!$(this).val()) {
+        $(this).addClass("is-invalid");
+        isFormValid = false;
+      }
+    });
+    return isFormValid;
+  }
 
-function removeFormErrors() {
-  contactForm.find("input,textarea").on("keydown", function () {
-    if ($(this).hasClass("is-invalid")) {
-      $(this).removeClass("is-invalid");
-    }
-  });
-}
+  function removeFormErrors() {
+    contactForm.find("input,textarea").on("keydown", function () {
+      if ($(this).hasClass("is-invalid")) {
+        $(this).removeClass("is-invalid");
+      }
+    });
+  }
 
-function clearForm() {
-  contactForm.find("input,textarea").each(function () {
-    $(this).val("");
-  });
-}
+  function clearForm() {
+    contactForm.find("input,textarea").each(function () {
+      $(this).val("");
+    });
+  }
 
-removeFormErrors();
+  removeFormErrors();
+
